9ebf1481df0937ff96facf0b354a1070494c2476
Gmajoulet <gmajoulet@gmail.com>
2015-03-27 00:55:57 +0100

    Fetching the data through the ItemCollection on form submit

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 (function () {
   class App {
-    constructor () {
+    constructor ($el) {
+      this.$el = $el;
       this.startListening();
     }
 
@@ -10,10 +11,51 @@
      * @return {App}
      */
     startListening () {
+      this.$('[data-name=search_form]').on('submit', this.onSearch.bind(this));
+
       return this;
+    }
+
+    /**
+     * On search form submit callback
+     * @param  {Event} event
+     */
+    onSearch (event) {
+      event.preventDefault();
+      var query = this.$('[data-name=search_input]').val();
+
+      this.search(query);
+    }
+
+    /**
+     * Instanciates a new ItemCollection if needed
+     *
+     * @param  {String} query
+     */
+    search (query) {
+      var collection = new ItemCollection();
+
+      collection.fetch(query)
+        .done(function () {
+          this.render(collection);
+        }.bind(this));
+    }
+
+    render (collection) {
+      // Render the collection
+    }
+
+    /**
+     * Search in the app scope only
+     *
+     * @param  {String} query
+     * @return {jQuery object}
+     */
+    $ (query) {
+      return $(query, this.$el);
     }
   }
 
   // Start the app
-  new App();
+  new App($('body'));
 }());
