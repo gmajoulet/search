e5f7656d2921965dbe111a6cb1cdba692192c866
Gmajoulet <gmajoulet@gmail.com>
2015-03-27 00:27:32 +0100

    Implementing a get attribute method on models

--- app/model.js
+++ app/model.js
@@ -8,4 +8,26 @@
     attributes = attributes || {};
     this.attributes = attributes;
   };
+
+  /**
+   * Homemade function to get a nested attribute without triggering an error
+   *
+   * @param  {String} path
+   * @return {Mixed} The attribute, or null
+   */
+  ItemModel.prototype.get = function (path) {
+    path = path.split('.');
+    var result = this.attributes,
+      i;
+
+    for (i = 0; i < path.length; i++) {
+      if (!result[path[i]]) {
+        return null;
+      }
+
+      result = result[path[i]];
+    }
+
+    return result;
+  };
 }());
