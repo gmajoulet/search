c399ea3f1bd0f26ce9e667b7058c614a826c9442
Gmajoulet <gmajoulet@gmail.com>
2015-03-27 02:21:28 +0100

    Testing the ItemModel

--- tests/tests.js
+++ tests/tests.js
@@ -2,6 +2,17 @@ QUnit.test('should find the ItemModel class', function (assert) {
   assert.ok('function' === typeof ItemModel);
 });
 
+QUnit.test('should test the ItemModel.get method', function (assert) {
+  var model = new ItemModel({ foo: { bar: 'baz' } });
+  // Should return the value (object). As a JSON because it's easier to check
+  assert.ok(JSON.stringify({ bar: 'baz' }) === JSON.stringify(model.get('foo')));
+  // Should return the nested value
+  assert.ok('baz' === model.get('foo.bar'));
+  // Should not fail
+  assert.ok(null === model.get('foo.bar.baz'));
+  assert.ok(null === model.get('qux'));
+});
+
 QUnit.test('should find the ItemCollection class', function (assert) {
   assert.ok('function' === typeof ItemCollection);
 });
